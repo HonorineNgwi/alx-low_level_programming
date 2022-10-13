58c237cae9057c9d5f0b6cc2f08f283a3a6ffd1b
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-13 16:02:04 -0700

    ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,5 +8,5 @@ int main(void)
 	printf("size of a long int: %lu byte(s)\n",
 	printf("size of a long long int: %lu byte(s)\n",
 	printf("size of a float: %lu byte(s)\n",
-	return (0);
+		return (0);
 }
