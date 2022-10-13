cfcb1bc7338fa9e55d5398db233261c59f2ab1b9
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-13 15:26:08 -0700

    ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * description - this is the start of the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
