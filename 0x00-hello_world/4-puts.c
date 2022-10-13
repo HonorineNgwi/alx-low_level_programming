7c749a274cad55f15881c1de5c3f1749189d73b5
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-13 15:03:32 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ * Description - this is the start of the program
+ */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle");
-		return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
