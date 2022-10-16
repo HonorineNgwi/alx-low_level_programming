e3bdd40914fc3b167cac49e86a4ab381e7065655
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-16 04:45:08 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - this is the main function
- * Description - this is the start of the program
+ * main - entry point
+ * Description:prints and that piece of art is
+ * useful" - Dora Korpar, 2015-10-19
+ * Return:1
  */
 int main(void)
 {
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-		return (0);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+
+		return (1);
 }
