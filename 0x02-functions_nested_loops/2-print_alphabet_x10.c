6b6cbe88d3598d75c19fa0cf2ea85eee10dca1c4
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-18 11:41:29 -0700

    ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,10 @@
 
 void print_alphabet_x10(void);
 {
-	int count = 1;
+	int count = 0;
 	char letter;
 
-	while (count++ <= 10)
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z' letter++)
 			_putchar(letter);
