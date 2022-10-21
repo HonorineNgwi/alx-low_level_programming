eadbe9a64ed8cd8f7388d4f049b8162669d3dff9
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-20 17:40:13 -0700

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - print 0123456789
  * owned by Bwave ICT
  * Return: void
  */
