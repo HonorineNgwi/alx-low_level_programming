8f7156815ff92074402ce8c92219e1f4991b2676
HonorineNgwi <tenenghonorine@gmail.com>
2022-10-24 14:22:39 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value to 98
+ * @n: data to change to 98
+ * Return:0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder is for arrays and strings in C
