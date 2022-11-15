7dd6102f80309766cb87198f8cb40a8bbde98aac
HonorineNgwi <tenenghonorine@gmail.com>
2022-11-15 06:16:39 -0800

    ok

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,17 @@
+#ifndef ABS
+#define ABS
+#include <stdio.h>
+
+/**
+ * main - computes the absolute value of a number x
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	printf("%d\n", ABS(x));
+	return (0);
+}
+#endif
