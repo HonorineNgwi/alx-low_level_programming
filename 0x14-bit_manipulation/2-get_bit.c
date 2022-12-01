a2860ec74ab83a179a755366d8d95f602c11a4ac
HonorineNgwi <tenenghonorine@gmail.com>
2022-12-01 12:07:11 -0800

    Done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i;
-	for (
+	unsigned long int div, res;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	div = 1 << index;
+	res = n & div;
+	if (res == div)
+		return (1);
+
+	return (0);
+}
